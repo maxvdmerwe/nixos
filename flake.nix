f69694b46afd050c7b16519c56292cc6531de615
Max van der Merwe <git@maxvdm.com>
2023-08-29 11:39:27 +0200

    Update to nixos unstable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,14 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    # Stable
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    # home-manager.url = "github:nix-community/home-manager/release-23.05";
+
+    # Unstable
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"
+    home-manager.url = "github:nix-community/home-manager/master";
+
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
