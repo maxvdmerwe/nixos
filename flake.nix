81b5255f6723f6a2cd9d9c4b3446361dec12f349
Max van der Merwe <git@maxvdm.com>
2023-08-29 11:44:22 +0200

    Fix missing colon in nixos-unstable.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     # home-manager.url = "github:nix-community/home-manager/release-23.05";
 
     # Unstable
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
 
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
