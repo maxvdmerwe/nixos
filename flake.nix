bbf55a2fe709ad48bc2bb4bc957e51e7011cd717
Max van der Merwe <git@maxvdm.com>
2023-10-01 18:47:18 +0200

    update flake.lock and change to stable pkgs

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   inputs = {
     # Stable
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    # home-manager.url = "github:nix-community/home-manager/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
 
     # Unstable
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # home-manager.url = "github:nix-community/home-manager/master";
 
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
